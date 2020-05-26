86bba4a500b230ea10f36d1029c3bc12e510f812
Marianela Iturriaga <mari.ytu@gmail.com>
2020-05-25 22:58:31 -0400

    Initial commit - Se agrega mockAPI. Se agregan nuevas acciones con flujo mediante Redux. Se separa el primer componente en presentation y container component.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import App from './components/App'
-import './index.css'
-import configureStore from './redux/configureStore'
-import { Provider as ReduxProvider } from 'react-redux'
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App";
+import "./index.css";
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
 
-const store = configureStore()
+const store = configureStore();
 
 render(
   <ReduxProvider store={store}>
@@ -15,5 +15,5 @@ render(
       <App />
     </Router>
   </ReduxProvider>,
-  document.getElementById('app')
-)
+  document.getElementById("app")
+);
